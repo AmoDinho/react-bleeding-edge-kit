8657b90197609c463fba1b314c0b3605027b60ec
AmoDinho <u14284783@tuks.co.za>
2020-05-10 20:24:53 +0200

    added the text component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import HeadingOne from "../components/HeadingOne";
 const Index = () => {
-  return <>This is the home page of the app!</>;
+  return (
+    <>
+      <HeadingOne className="text-center text-blue-500">
+        This is the home page of the app!
+      </HeadingOne>
+    </>
+  );
 };
 
 export default Index;
