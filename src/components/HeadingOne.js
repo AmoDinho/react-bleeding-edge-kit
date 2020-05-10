9869f9c93e44d88bb3a2b1d1b42d104658ff5aca
AmoDinho <u14284783@tuks.co.za>
2020-05-10 20:21:38 +0200

    added proptypes

--- src/components/HeadingOne.js
+++ src/components/HeadingOne.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 const HeadingOne = ({ className, children }) => {
   return <h1 className={`${className} text-xl`}>{children}</h1>;
 };
 
+HeadingOne.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+HeadingOne.defaultProps = {
+  className: "",
+};
 export default HeadingOne;
